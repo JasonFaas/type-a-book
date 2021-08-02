1cb6f12465ef5398a450d599ce3e71191a143653
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2021-08-02 15:26:03 -0700

    Add first unit test

---
+++ type-a-book/tests/test_hello_world.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+class TestHelloWorld(TestCase):
+
+    def test_hello_world_pass(self):
+        assert "hello" != "world"
+
+    def test_hello_world_fail(self):
+        self.assertEqual("hello", "world")
