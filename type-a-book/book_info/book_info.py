e9b411b3d42da13dedc03c37c36645e6c1071aff
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2021-08-02 20:39:17 -0700

    Get chapter list from downloaded file

--- type-a-book/book_info/book_info.py
+++ type-a-book/book_info/book_info.py
@@ -3,12 +3,42 @@ import os
 class BookInfo():
 
     def book_list(self):
-        folder = "../resources/books/project_gutenberg/"
+        folder = self.path_for_books()
         subfolders = [f.name for f in os.scandir(folder) if f.is_dir() ]
         return subfolders
 
+    def chapter_list_new(self, book_name):
+        book_file_name = self.book_file_name(book_name)
+        with open("{}/{}".format(self.path_for_book(book_name), book_file_name), 'r') as file:
+            data = file.read()
+
+        book_by_line = data.split('\n')
+        contents_line_idx = 0
+        while not book_by_line[contents_line_idx].startswith("Contents"):
+            contents_line_idx += 1
+        if contents_line_idx > len(book_by_line) / 2:
+            return ""
+
+        last_chapter_idx = contents_line_idx + 2
+        while not book_by_line[last_chapter_idx] == "":
+            last_chapter_idx += 1
+        if last_chapter_idx > len(book_by_line) / 2:
+            return ""
+
+        chapters_unformatted = book_by_line[contents_line_idx + 2:last_chapter_idx]
+
+        for idx in range(len(chapters_unformatted)):
+            chapters_unformatted[idx] = chapters_unformatted[idx][1:]
+
+        return chapters_unformatted
+
+    def book_file_name(self, book_name):
+        folder = "../resources/books/project_gutenberg/{}".format(book_name)
+        files = [f.name for f in os.scandir(folder) if f.is_file() and f.name[0].isdigit()]
+        return files[0]
+
     def chapter_list(self, book_name):
-        folder = "../resources/books/project_gutenberg/" + book_name + "/chapters/"
+        folder = self.path_for_book(book_name) + "/chapters/"
         files = []
         if os.path.exists(folder):
             files = [f.name.replace(".txt", "") for f in os.scandir(folder) if (f.is_file() and f.name[0] == '0')]
@@ -16,7 +46,7 @@ class BookInfo():
         return files
 
     def chapter_of_book(self, book_name, chapter):
-        folder = "../resources/books/project_gutenberg/" + book_name + "/chapters/"
+        folder = self.path_for_book(book_name) + "/chapters/"
         file = folder + chapter + ".txt"
         with open(file, 'r') as file:
             data = file.read()
@@ -25,6 +55,12 @@ class BookInfo():
         data = data.replace(chr(8220), "\"").replace(chr(8221), "\"")
 
         return data
+
+    def path_for_book(self, book_name):
+        return self.path_for_books() + book_name
+
+    def path_for_books(self):
+        return "../resources/books/project_gutenberg/"
 
     @staticmethod
     def verify_legal_characters(text):
--- type-a-book/tests/test_book_info.py
+++ type-a-book/tests/test_book_info.py
@@ -16,6 +16,51 @@ class TestBookInfo(unittest.TestCase):
                                                   "02-Chapter_2",
                                                   "03-Chapter_3"]
 
+    def test_file_name_of_book(self):
+        actual_file_name = self.book_info.book_file_name("Peter_Pan")
+        assert actual_file_name == "16-0.txt", actual_file_name
+
+    def test_peter_pan_chapter_list_new(self):
+        chapter_list_new = self.book_info.chapter_list_new("Peter_Pan")
+        expected_chapter_list = [
+            "Chapter I. PETER BREAKS THROUGH",
+            "Chapter II. THE SHADOW",
+            "Chapter III. COME AWAY, COME AWAY!",
+            "Chapter IV. THE FLIGHT",
+            "Chapter V. THE ISLAND COME TRUE",
+            "Chapter VI. THE LITTLE HOUSE",
+            "Chapter VII. THE HOME UNDER THE GROUND",
+            "Chapter VIII. THE MERMAIDS’ LAGOON",
+            "Chapter IX. THE NEVER BIRD",
+            "Chapter X. THE HAPPY HOME",
+            "Chapter XI. WENDY’S STORY",
+            "Chapter XII. THE CHILDREN ARE CARRIED OFF",
+            "Chapter XIII. DO YOU BELIEVE IN FAIRIES?",
+            "Chapter XIV. THE PIRATE SHIP",
+            "Chapter XV. “HOOK OR ME THIS TIME”",
+            "Chapter XVI. THE RETURN HOME",
+            "Chapter XVII. WHEN WENDY GREW UP",
+        ]
+        assert chapter_list_new == expected_chapter_list, chapter_list_new
+
+    def test_alice_adventure_chapter_list_new(self):
+        chapter_list_new = self.book_info.chapter_list_new("Alices_Adventures_in_Wonderland")
+        expected_chapter_list = [
+            "CHAPTER I.     Down the Rabbit-Hole",
+            "CHAPTER II.    The Pool of Tears",
+            "CHAPTER III.   A Caucus-Race and a Long Tale",
+            "CHAPTER IV.    The Rabbit Sends in a Little Bill",
+            "CHAPTER V.     Advice from a Caterpillar",
+            "CHAPTER VI.    Pig and Pepper",
+            "CHAPTER VII.   A Mad Tea-Party",
+            "CHAPTER VIII.  The Queen’s Croquet-Ground",
+            "CHAPTER IX.    The Mock Turtle’s Story",
+            "CHAPTER X.     The Lobster Quadrille",
+            "CHAPTER XI.    Who Stole the Tarts?",
+            "CHAPTER XII.   Alice’s Evidence",
+        ]
+        assert chapter_list_new == expected_chapter_list, chapter_list_new
+
     def test_verify_all_books_all_legal_characters(self):
         # TODO make this more efficient
         for book in self.book_info.book_list():
